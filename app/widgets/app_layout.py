baeb157185bc40edaa6a6fdbabf80d2680dfc051
Serein <108068915+cafesuada24@users.noreply.github.com>
2023-11-27 10:52:09 +0700

    Implement abstract class app_frame

--- app/widgets/app_layout.py
+++ app/widgets/app_layout.py
@@ -1,23 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
+from ..singleton import SingletonMeta
 from .. import CONFIG
-from ..namespaces.widgets import Widgets
-from ..namespaces.tk_variable import TkVariable
-from ..namespaces.database import Database as DB
+from .head_bar import HeadBar
+from .video_browser import VideoBrowser
+from .footer import Footer
 
-COLUMNS = CONFIG['display']['columns']
 
-class MainLayout(ttk.Frame):
+class MainLayout(ttk.Frame, metaclass=SingletonMeta):
     def __init__(self, root):
         super().__init__(root)
-        Widgets(self)
 
-        # Search engine init
-        # _data = []
-        # for item in General.data:
-        #     _data.extend(((item.get_name().lower(), item.id), (item.get_director().lower(), item.id)))
-        
         self._head_bar = None
         self._rpanel = None
         self._footer = None
@@ -38,18 +32,23 @@ class MainLayout(ttk.Frame):
             self._rpanel.grid_forget()
         if self._footer:
             self._footer.grid_forget()
-        self._head_bar = head_bar or Widgets().HEAD_BAR
-        self._browser = browser or Widgets().BROWSER
+        self._head_bar = head_bar or HeadBar()
+        self._browser = browser or VideoBrowser()
         self._rpanel = rpanel
-        self._footer = footer or Widgets().FOOTER
+        self._footer = footer or Footer()
         self.__layout()
         self.grid(row=0, column=0, sticky=tk.NSEW)
 
     def __layout(self):
-        self._head_bar.grid(row=0, column=0, sticky=tk.NSEW)
-        self._browser.grid(row=1, column=0, sticky=tk.NSEW)
-        self._rpanel.grid(row=0, column=1, rowspan=2, sticky=tk.NSEW)
-        self._footer.grid(row=2, column=0, columnspan=2, sticky=tk.NSEW)
-        for children in (self._head_bar, self._browser, self._rpanel, self._footer):
+        self._head_bar.grid(row=0, column=0, sticky='nsew')
+        self._browser.grid(row=1, column=0, sticky='nsew')
+        self._rpanel.grid(row=0, column=1, rowspan=2, sticky='nsew')
+        self._footer.grid(row=2, column=0, columnspan=2, sticky='nsew')
+        for children in (
+            self._head_bar,
+            self._browser,
+            self._rpanel,
+            self._footer,
+        ):
             children.grid(padx=5, pady=5)
-        
+        pass
