962b88c2d22338a85d1dd4e1d9c5335b7dc45db1
Serein <108068915+cafesuada24@users.noreply.github.com>
2023-12-01 10:11:48 +0700

    Add scrollbar to video browser

--- app/widgets/video_browser.py
+++ app/widgets/video_browser.py
@@ -15,9 +15,14 @@ class VideoBrowser(AppFrame, metaclass=SingletonMeta):
     def __init__(self, root):
         super().__init__(root)
 
+        for children in self.winfo_children():
+            children.grid(padx=0)
+
     def _create_widgets(self):
         columns_width = (50, 300, 300, 150)
         self.__browser = ttk.Treeview(self, show='headings', height=20)
+        self.__scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.__browser.yview)
+        self.__browser['yscrollcommand'] = self.__scrollbar.set
         self.__browser['columns'] = tuple(
             VideosDB.COLUMNS[column] for column in self.COLUMNS
         )
@@ -33,24 +38,29 @@ class VideoBrowser(AppFrame, metaclass=SingletonMeta):
 
     def _display_widgets(self):
         self.__browser.grid(row=0, column=0, sticky='nsew')
+        self.__scrollbar.grid(row=0, column=1, sticky='nsew')
 
     def __clear_contents(self):
         for item in self.__browser.get_children():
             self.__browser.delete(item)
-
+    
     def __item_selected(self, *ignore):
-        selected = self.__browser.focus()
-        selected = self.__browser.item(selected)['values']
-        if not len(selected):
+        selected = self.__browser.selection()
+        if not selected:
             return
-        TkVariable().selected_id.set(selected[0])
+        id = TkVariable().get_selected_id(display_msg=False)
+        if id != selected[0]:
+            TkVariable().selected_id.set(selected[0])
 
     def display_playlist(self) -> None:
         contents = EventHandlers.get_brower_items()
         if not contents:
             return
         self.__clear_contents()
-        for content in contents:
+        for item in contents:
             self.__browser.insert(
-                '', tk.END, values=content.list_all(self.COLUMNS)
+                '', tk.END, id=item.get_id(), values=item.list_all(self.COLUMNS)
             )
+        
+        first_element = int(self.__browser.get_children()[0])
+        self.__browser.selection_set(first_element)
