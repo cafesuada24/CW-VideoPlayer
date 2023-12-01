a5898177eb57c055ff9d428187c061b66098de97
Serein <108068915+cafesuada24@users.noreply.github.com>
2023-12-02 04:01:11 +0700

    Update path param in VideosDB

--- app/core/videos_db.py
+++ app/core/videos_db.py
@@ -22,8 +22,8 @@ class VideosDB(metaclass=SingletonMeta):
 
     TABLE = 'videos'
 
-    def __init__(self):
-        db_path = Path(CONFIG['path']['db'])  # get database path from config
+    def __init__(self, db_path=None):
+        db_path = db_path or Path(CONFIG['path']['db'])  # get database path from config
         if not db_path.exists():
             db_path.parent.mkdir(parents=True, exist_ok=True)
             db_path.touch()
