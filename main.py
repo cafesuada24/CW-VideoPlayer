f17f6a411c1af4940aaefa4ffb52a24b7595aa0c
Serein <108068915+cafesuada24@users.noreply.github.com>
2023-12-01 13:43:56 +0700

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+"""
+Video Player main module
+"""
 from app.video_player import VideoPlayer
 from app.core.videos_db import VideosDB
 
-if __name__ == "__main__":
+
+def main():
     videoplayer = VideoPlayer()
     videoplayer.mainloop()
     VideosDB().close()
+
+if __name__ == "__main__":
+    main()    
